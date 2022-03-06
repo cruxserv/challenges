69c9cfcb4cd4ebea3ed05c1221bd16de4675fe3c
John-Carlos Sanabria <johncarlos.sanabria@gmail.com>
2022-03-06 15:08:23 -0800

    3-06-22 update

---
+++ reversestring.py
@@ -0,0 +1,12 @@
+def reversestring(a):
+    new = list(a)
+    new.reverse()
+    print("".join(new))
+
+
+def rstr(a):
+    print(a[::-1])
+
+
+# reversestring("Hello World!")
+# rstr("Hallow World?")
