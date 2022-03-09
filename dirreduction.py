ed7eb8bc72c3e9cfe8eda8fe3c4efbcdfe0d922b
John-Carlos Sanabria <johncarlos.sanabria@gmail.com>
2022-03-09 13:40:38 -0800

    3-09-22.3 update

--- dirreduction.py
+++ dirreduction.py
@@ -3,11 +3,10 @@ def dirReduc(a):
     stack = []
 
     for d in a:
-    if d in dict:
-        if stack and stack[-1] == dict[d]:
-            stack.pop()
-        else:
-            stack.append(d)
+        if d in dict:
+            if stack and stack[-1] == dict[d]:
+                stack.pop()
+            else:
+                stack.append(d)
 
-
-return stack
+    return stack
--- scrambles.py
+++ scrambles.py
@@ -1,17 +1,5 @@
-from collections import Counter
-
-
 def scramble(s1, s2):
-    s1 = list(s1)
-    s2 = list(s2)
-    for i in s2:
-        print(i)
-        if s2.count(i) <= s1.count(i):
-            continue
-        else:
-            print(False)
-            break
-        print(True)
-
-
-scramble('cedewaraaossoqqyt', 'codewars')
+    for l in set(s2):
+        if s2.count(l) > s1.count(l):
+            return False
+    return True
