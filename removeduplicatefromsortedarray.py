0d19ef661ef78866426e26915d69b346d6820746
John-Carlos Sanabria <johncarlos.sanabria@gmail.com>
2024-02-09 08:04:19 -0800

    made 26 faster

--- removeduplicatefromsortedarray.py
+++ removeduplicatefromsortedarray.py
@@ -1,15 +1,19 @@
 def removeDuplicates(nums):
     if not nums:
         return 0
-
-    # Initialize the counter and the second pointer.
+    
+    # Initialize the counter at the first element.
     i = 0
     
+    # Start the loop from the second element.
     for j in range(1, len(nums)):
-        # If the current element is not equal to the previous element,
-        # increment the counter and update the nums array.
+        # Only if we find a new element (not a duplicate)
         if nums[j] != nums[i]:
+            # Increment the counter.
             i += 1
-            nums[i] = nums[j]
-    # The length of the unique elements is i + 1
+            # Only perform the assignment if it's needed.
+            if i != j:
+                nums[i] = nums[j]
+                
+    # The length of the unique elements is i + 1.
     return i + 1
